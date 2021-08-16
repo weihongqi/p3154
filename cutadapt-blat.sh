6a843dbd86da1a49c88d053cc90c29c43793f4ef
weihongqi <weihong.qi@fgcz.ethz.ch>
2021-08-16 13:44:07 +0200

    Update cutadapt-blat.sh

--- cutadapt-blat.sh
+++ cutadapt-blat.sh
@@ -3,6 +3,9 @@ source ${CONDA_BASE}/etc/profile.d/conda.sh
 conda activate cutadapt
 module load Tools/UCSC/399
 
+wget http://ftp.flybase.net/genomes/Drosophila_melanogaster/dmel_r6.32_FB2020_01/fasta/dmel-all-gene-r6.32.fasta.gz
+gunzip -d dmel-all-gene-r6.32.fasta.gz
+
 SAMPLES=$(ls lima_output.*.bam.Q20.fastq)
 for IFILE in ${SAMPLES[@]}
 do
@@ -12,7 +15,7 @@ do
     #cutadapt -a "A{500}" -e 0.01 -O 1 --untrimmed-output $NAME\.no_tail.fastq --info-file $NAME\.tails.txt -o $NAME\.trimtail.fastq --fasta $NAME\.cutadapt.fastq
     cutadapt -a "A{500}" -e 0.01 -O 1 --info-file $NAME\.tails.txt -o $NAME\.trimtail.fastq $NAME\.cutadapt.fastq
     /usr/local/ngseq/packages/Tools/EMBOSS/6.6.0/bin/seqret $NAME\.trimtail.fastq $NAME\.trimtail.fasta
-    blat -t=DNA -q=RNA /srv/GT/analysis/qiwei/p3154/o6676_FLAMseq/dmel_r6.32_FB2020_01/dmel-all-gene-r6.32.fasta  $NAME\.trimtail.fasta  $NAME\.trimtail.psl
+    blat -t=DNA -q=RNA dmel-all-gene-r6.32.fasta  $NAME\.trimtail.fasta  $NAME\.trimtail.psl
     pslCDnaFilter -globalNearBest=0  $NAME\.trimtail.psl  $NAME\.trimtail.best.psl 
     pslCDnaFilter -uniqueMapped $NAME\.trimtail.best.psl $NAME\.trimtail.best.unique.psl
 done
